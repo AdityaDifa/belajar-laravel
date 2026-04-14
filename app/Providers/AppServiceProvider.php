8c36b5e919b460d51b5d04b6e1918542c873bb46
Aditya Difa Amanullah <adityadifa123@gmail.com>
2026-04-14 13:07:56 +0700

    final v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        if (config('app.env') === 'production' || $this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 }
